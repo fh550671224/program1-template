05146be5172d02791b5c20560e46306cd0f4d45e
Hao Fu <hfu11@binghamton.edu>
2019-02-18 14:23:06 -0500

    add list

--- List.cpp
+++ List.cpp
@@ -1,1 +1,27 @@
-wfwefwwef
+#include "List.h"
+#include <iostream>
+
+
+List::List(){
+    head=NULL;
+    tail=NULL;
+    index=0;
+}
+
+List::~List(){
+    Node * node = this->head;
+    while(node != NULL){
+        delete node->data;
+    }
+    node->=this->head;
+    Node * node_next;
+    while(node != NULL){
+        node_next = node->next;
+        delete node;
+        node=node->next;
+    }
+}
+
+void List::insert(int index, Planet * p){
+  
+}
--- List.h
+++ List.h
@@ -1,4 +1,25 @@
 #ifndef LIST_H
 #define LIST_H
 
+class List{
+
+    private:
+    int index;
+    Node * head, tail;
+
+    public:
+    List();
+    ~List();
+    void insert(int index, Planet * p);
+    Planet* read(int index);
+    bool remove(int index);
+    unsigned size();
+}
+
+class Node{
+    private:
+        Planet * data;
+        Node * next, prev;
+}
+
 #endif
